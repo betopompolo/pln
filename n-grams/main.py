94b21c2a0bb96a7ea86c9567ac7562a290df09c9
Adalberto Pompolo <adalberto.pompolo@taqtile.com.br>
2021-08-01 23:00:51 -0300

    feat: vocabulary

--- n-grams/main.py
+++ n-grams/main.py
@@ -1,8 +1,13 @@
 from dataset_parser import load_dataset, extract_from_tag, remove_punctuation, remove_linebreak
+from n_grams import get_vocabulary, sum_vocabularies, word_count
 
 if __name__ == '__main__':
-    for content in load_dataset('folha94'):
+    dataset_name = 'folha95'
+    corpus_vocabulary = {}
+    for content in load_dataset(dataset_name):
         text = extract_from_tag(content, 'TEXT')
-        text = remove_punctuation(text)
-        text = remove_linebreak(text)
-        print(text + '\n--------')
+        formatted_text = remove_linebreak(remove_punctuation(text))
+        vocabulary = get_vocabulary(text)
+        corpus_vocabulary = sum_vocabularies(corpus_vocabulary, vocabulary)
+
+    print(f'word count for {dataset_name} is {word_count(corpus_vocabulary)}')
--- n-grams/n_grams.py
+++ n-grams/n_grams.py
@@ -1,4 +1,37 @@
+import functools
+import operator
+
+Vocabulary = dict[str, int]
+
+
 def n_gram(text: str, n: int) -> zip:
-    split = text.split(' ')
+    split = text.split()
     grams = [split[i:] for i in range(n)]
     return zip(*grams)
+
+
+def get_vocabulary(text: str) -> Vocabulary:
+    vocabulary = {}
+    for word in text.split():
+        if vocabulary.get(word) is None:
+            vocabulary[word] = 1
+        else:
+            vocabulary[word] += 1
+
+    return vocabulary
+
+
+def sum_vocabularies(v1: Vocabulary, v2: Vocabulary) -> Vocabulary:
+    result = v1.copy()
+
+    for key in v2.keys():
+        if result.get(key) is None:
+            result[key] = v2[key]
+        else:
+            result[key] += v2[key]
+
+    return result
+
+
+def word_count(v: Vocabulary) -> int:
+    return functools.reduce(operator.add, v.values())
